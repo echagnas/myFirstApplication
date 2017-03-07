46cd6ee0de3340d2209bf26b76e30c5283e7f410
Emmanuel Chagnas <emmanuelchagnas@gmail.com>
2017-03-07 09:15:54 +0100

    Begin custom control

---
+++ myFirstApplication/RatingControl.swift
@@ -0,0 +1,29 @@
+//
+//  RatingControl.swift
+//  myFirstApplication
+//
+//  Created by Emmanuel Chagnas on 07/03/2017.
+//  Copyright © 2017 Emmanuel Chagnas. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+  //MARK: Initialization
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+}
